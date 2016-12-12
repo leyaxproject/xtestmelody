b77431aae0b64e1342446044196b9e259670cc80
leyaxproject <hsantos@leya.com>
2016-12-12 16:21:54 +0000

    Delete strings2.go

--- strings2.go
+++
@@ -1,8 +0,0 @@
-package xstringutils
-
-func FullName(f, l string) (string, int) {
-	full := f + " " + l + " from github package xstringutils UPDATED 2."
-	length := len(full)
-
-	return full, length
-}
