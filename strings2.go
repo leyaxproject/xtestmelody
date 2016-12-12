9de8567f10b95f6b4c5bd5635c1c1778b50b1e44
leyaxproject <hsantos@leya.com>
2016-12-12 15:51:45 +0000

    Update and rename strings.go to strings2.go

--- strings2.go
+++ strings2.go
@@ -1,7 +1,7 @@
 package xstringutils
 
 func FullName(f, l string) (string, int) {
-	full := f + " " + l + " from github package xstringutils UPDATED."
+	full := f + " " + l + " from github package xstringutils UPDATED 2."
 	length := len(full)
 
 	return full, length
